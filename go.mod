062f0902851f946f75e697717c1d1145555ca113
zaenalarifin12 <zaenalblaze@gmail.com>
2021-10-13 10:07:48 +0700

    testing dependecies

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/zaenalarifin12/app-say-hello
+
+go 1.17
+
+require github.com/zaenalarifin12/go-say-hello/v2 v2.0.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	go_say_hello "github.com/zaenalarifin12/go-say-hello/v2"
+)
+
+func main()  {
+	fmt.Println(go_say_hello.SayHello("name"))
+}
+
